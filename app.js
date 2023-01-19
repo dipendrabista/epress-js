67d3ff0d76fdc6b5678e404371612c84516ae7c7
Bista, Dipendra <Dipendra.Bista@cotiviti.com>
2023-01-19 14:17:07 +0545

    Custom middleware git add .

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const port = 3000
 //serving static contents like html,css,js,image
 app.use(express.static("public"))
 
+// custom middleware
+const myMiddleware = (req, res, next) => {
+    console.log("Request from :" + req.url);
+    next();
+};
+app.use(myMiddleware)
+
+
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
