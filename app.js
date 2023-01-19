147180af4c2d279dae5d139d09bc2e9847754120
Bista, Dipendra <Dipendra.Bista@cotiviti.com>
2023-01-19 14:09:13 +0545

    serving static contents using built-in middleware

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
+//serving static contents like html,css,js,image
+app.use(express.static("public"))
 
 app.get('/', (req, res) => {
     res.send('Hello World')
